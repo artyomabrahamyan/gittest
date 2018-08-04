ae4fcb82221182460f9e9322663156ec6b5563ff
Artyom Abrahamyan <artyom.abrahamyan@optym.com>
2018-08-04 22:20:02 +0400

    2nd branch Some method added

--- SampleProj/Class1.cs
+++ SampleProj/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace SampleProj
 {
     class Class1
     {
+        public static void Some()
+        {
+            Console.WriteLine("Bye");
+        }
     }
 }
